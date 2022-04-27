205080bf5408be6cc76bf65eb3dc9553ffa041c9
zkabyken <zhalyn2001@gmail.com>
2022-04-26 18:54:03 -0700

    Organized initial classes

---
+++ app/src/main/java/com/example/zooseeker/AnimalListActivity.java
@@ -0,0 +1,14 @@
+package com.example.zooseeker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AnimalListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animal_list);
+    }
+}
