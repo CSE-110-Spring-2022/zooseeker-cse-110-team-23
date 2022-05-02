c1b1cf8b02f324193fc5d771b5f48d9113ea0acb
mhuzaifa125 <97709057+mhuzaifa125@users.noreply.github.com>
2022-05-01 21:08:48 -0700

    Added UI tests for planner

---
+++ app/src/androidTest/java/com/example/zooseeker/AnimalListEditTest.java
@@ -0,0 +1,114 @@
+package com.example.zooseeker;
+
+
+import androidx.test.espresso.DataInteraction;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import static androidx.test.InstrumentationRegistry.getInstrumentation;
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
+import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static androidx.test.espresso.action.ViewActions.*;
+import static androidx.test.espresso.assertion.ViewAssertions.*;
+import static androidx.test.espresso.matcher.ViewMatchers.*;
+
+import com.example.zooseeker.R;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class AnimalListEditTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void animalListEditTest() {
+        ViewInteraction materialButton = onView(
+allOf(withId(R.id.plan_btn), withText("PLAN"),
+childAtPosition(
+childAtPosition(
+withId(android.R.id.content),
+0),
+3),
+isDisplayed()));
+        materialButton.perform(click());
+        
+        ViewInteraction appCompatEditText = onView(
+allOf(withId(R.id.new_todo_text),
+childAtPosition(
+childAtPosition(
+withId(android.R.id.content),
+0),
+4),
+isDisplayed()));
+        appCompatEditText.perform(replaceText("Zebra"), closeSoftKeyboard());
+        
+        ViewInteraction materialButton2 = onView(
+allOf(withId(R.id.add_todo_btn), withText("Add"),
+childAtPosition(
+childAtPosition(
+withId(android.R.id.content),
+0),
+5),
+isDisplayed()));
+        materialButton2.perform(click());
+        
+        ViewInteraction appCompatEditText2 = onView(
+allOf(withId(R.id.animal_item_text), withText("Zebra"),
+childAtPosition(
+childAtPosition(
+withId(R.id.animal_items),
+0),
+0),
+isDisplayed()));
+        appCompatEditText2.perform(replaceText("Elephant"));
+        
+        ViewInteraction appCompatEditText3 = onView(
+allOf(withId(R.id.animal_item_text), withText("Elephant"),
+childAtPosition(
+childAtPosition(
+withId(R.id.animal_items),
+0),
+0),
+isDisplayed()));
+        appCompatEditText3.perform(closeSoftKeyboard());
+        }
+    
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup)parent).getChildAt(position));
+            }
+        };
+    }
+    }
