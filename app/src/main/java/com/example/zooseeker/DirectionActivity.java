6eaa8e2eb18b2c8b5fa346ffa1ef37a6a76d62c6
rloeza <rloeza@ucsd.edu>
2022-05-07 23:04:41 -0700

    Fixed path bug

--- app/src/main/java/com/example/zooseeker/DirectionActivity.java
+++ app/src/main/java/com/example/zooseeker/DirectionActivity.java
@@ -37,7 +37,7 @@ public class DirectionActivity extends AppCompatActivity {
         List<AnimalListItem> animalPlanItems = AnimalListDatabase.getSingleton(this).animalListItemDao().getAll();//this.viewModel.getAnimalListItems().getValue();
 
         Graph<String, IdentifiedWeightedEdge> g = ZooData.loadZooGraphJSON(this,"sample_zoo_graph.json");
-        goal = animalPlanItems.get(0).animal_id;
+        goal = animalPlanItems.get(0).animal_id;        // save first animal in plan as goal
         GraphPath<String, IdentifiedWeightedEdge> path;
 
         Map<String, ZooData.VertexInfo> vInfo = ZooData.loadVertexInfoJSON(this,"sample_node_info.json");
@@ -45,25 +45,49 @@ public class DirectionActivity extends AppCompatActivity {
 
         log = new ArrayList<>(1);
 
+        String prev = "entrance_exit_gate";     // my code
         for(int i=0; i<animalPlanItems.size(); i++) {
 
             path = DijkstraShortestPath.findPathBetween(g,start, goal);
             int j = 1;
             String b = "";
+
             for (IdentifiedWeightedEdge e : path.getEdgeList()) {
+                // my code
+                // if the target of next edge is the prev place
+                if(prev == vInfo.get(g.getEdgeTarget(e).toString()).name) {
+                    String from = vInfo.get(g.getEdgeTarget(e).toString()).name;
+//                    String from = "deez nuts";
+                    String to = vInfo.get(g.getEdgeSource(e).toString()).name;
+                    double length = g.getEdgeWeight(e);
+                    String street = eInfo.get(e.getId()).street;
 
-                String from = vInfo.get(g.getEdgeSource(e).toString()).name;
-                double length = g.getEdgeWeight(e);
-                String to = vInfo.get(g.getEdgeTarget(e).toString()).name;
-                String street = eInfo.get(e.getId()).street;
+                    b += j + ". Walk "+ length +" meters along "+ street +" from "+ from +" to  "+to +"\n";
+                    j++;
 
-                b += j + ". Walk "+ length +" meters along "+ street +" from "+from +" to  "+to +"\n" ;
+                    // update prev
+                    prev = to;
+                }
+                else {
+                    String from = vInfo.get(g.getEdgeSource(e).toString()).name;
+//                    String from = "deez nuts";
+                    String to = vInfo.get(g.getEdgeTarget(e).toString()).name;
+                    double length = g.getEdgeWeight(e);
+                    String street = eInfo.get(e.getId()).street;
 
-                j++;
+                    b += j + ". Walk "+ length +" meters along "+ street +" from "+ from +" to  "+to +"\n" ;
+                    j++;
+
+                    // update prev
+                    prev = to;
+                }
+                // end of my code
+//                b += j + ". Walk "+ length +" meters along "+ street +" from "+ from +" to  "+to +"\n" ;
+//                j++;
             }
             log.add(b);
             start = animalPlanItems.get(i).animal_id;
-            if((i+1) == animalPlanItems.size()) {
+            if((i+1) == animalPlanItems.size()) {       // if reached end of animals list
                 break;
             }
             goal = animalPlanItems.get(i+1).animal_id;
