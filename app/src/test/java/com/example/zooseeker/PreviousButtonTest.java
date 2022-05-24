6cb51a2fb6147f09a718d8115daff7e323300024
Kai-Wen Cheng <kac003@ucsd.edu>
2022-05-24 15:45:47 -0700

    Previous Button Testing

---
+++ app/src/test/java/com/example/zooseeker/PreviousButtonTest.java
@@ -0,0 +1,28 @@
+package com.example.zooseeker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class PreviousButtonTest {
+    @Test
+    public void PreviousButtonTrue() {
+        int Btn = R.id.prev_animal_btn;
+        assertNotNull(Btn);
+    }
+}
